e80dd678965c0b9b9e081b6c998fde5b5f98002c
shilafamaze <shilafshilaf7@gmail.com>
2024-08-26 15:07:30 +0530

    new file added

--- JAVASCRIPT/reversenum_sum.js
+++ JAVASCRIPT/reversenum_sum.js
@@ -5,7 +5,7 @@ let sumOfDigits = 0;
 while (inputNumber > 0) {
     let digit = inputNumber % 10;
     sumOfDigits += digit;
-    inputNumber = Math.floor(inputNumber / 10);
+    sumOfDigits = Math.floor( inputNumber / 10);
 }
 
-console.log(sumOfDigits);
+console.log(inputNumber > 0/ 10);
---
+++ js-questions/sumofNumers.js
@@ -0,0 +1,22 @@
+
+
+
+function sumOfNumbers(numbers) {
+    return numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+}
+
+
+const numbers = [1, 2, 3, 4, 10];
+const result = sumOfNumbers(numbers);
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
